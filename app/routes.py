39a7a1028ec89da17093a11076fb85a01f2e9a3e
Varun Iyer <varun_iyer@protonmail.com>
2019-12-01 22:39:16 -0800

    Added txt retrieval

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ import os
 import flask
 from urllib.parse import quote, unquote
 from app.models import Document
-from app.ref import build_graph, graph_svg
+from app.ref import build_graph, graph_svg, sort_graph
 from app import app, db
 from app.forms import GraphForm
 
@@ -24,3 +24,17 @@ def results(doi):
 @app.route('/cy', methods=['GET'])
 def cy():
     return flask.render_template('cy.html')
+     
+@app.route('/txt/<doi>', methods=['GET'])
+def txt(doi):
+    g = build_graph(unquote(doi))
+    s = sort_graph(g)
+    ref = [a.refstring() for a in s]
+    cb = []
+    for a in s:
+        try:
+            cb.append(a['is-referenced-by-count'])
+        except KeyError:
+            cb.append('?')
+    lines = ['{}\t{}'.format(r, c) for r, c in zip(ref, cb)]
+    return '\n'.join(lines)
